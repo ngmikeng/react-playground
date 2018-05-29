5446df5b0e75a622e032bfc75666fd5d3411b685
Minh Nguyen <ngmikeng@gmail.com>
2018-05-29 09:54:18 +0700

    Fix chrome redux extension.

--- redux-tut-api-jsonplaceholder/src/App.js
+++ redux-tut-api-jsonplaceholder/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
@@ -11,10 +10,6 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header> */}
           <PostForm />
           <hr />
           <Posts />
